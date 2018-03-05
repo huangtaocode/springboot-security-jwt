bf5e34abbd63499abd4e94133979b3375898b748
huangtaowork <361299045@qq.com>
2018-03-05 14:18:31 +0800

    add role

--- src/main/java/codetao/controller/user/RoleController.java
+++ src/main/java/codetao/controller/user/RoleController.java
@@ -4,28 +4,28 @@ import codetao.domain.Role;
 import codetao.service.RoleService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Map;
 
 /**
  * Created by taohuang on 2018/3/2.
  */
-@Controller
-@RequestMapping("role")
+@RestController
+@RequestMapping("/role")
 public class RoleController {
 
     @Autowired
     private RoleService roleService;
 
-    @RequestMapping("/list")
-    @ResponseBody
+    @GetMapping("/list")
     public List<Role> list(){
-
-
-
-
         return roleService.findAll();
+    }
+
+    @PostMapping("/create")
+    public void create(@RequestBody Map<String, Object> map){
+        System.out.println(map);
     }
 }
